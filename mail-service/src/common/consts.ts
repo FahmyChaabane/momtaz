6df50c044b499d4aaa0f8eaab2c8f8c58b708899
Fahmi Chaabane <chaabane.fahmi@outlook.fr>
2022-09-21 02:06:18 +0100

    moving mailing service

---
+++ mail-service/src/common/consts.ts
@@ -0,0 +1,18 @@
+// queues names
+export const MAIL_SERVICE = 'mail-service';
+
+// jobs names
+export const REGISTER_JOB = 'register-job';
+export const FORGOT_PWD_JOB = 'forgot-pwd-job';
+export const RESET_PWD_CONFIRMATION_JOB = 'reset-pwd-confirmation-job';
+
+// templates names
+export const REGISTRATION_CONFIRMATION_TEMPLATE = 'registerconfirmation';
+export const FORGOT_PASSWORD_TEMPLATE = 'forgotpwd';
+export const RESET_PWD_CONFIRMATION_TEMPLATE = 'resetpwdconfirmation';
+
+// mail subject names
+export const REGISTRATION_CONFIRMATION_SUBJECT =
+  'Momtaz Registration Confirmation ✔';
+export const RESET_PASSWORD_SUBJECT = 'Reset Password Request ✔';
+export const RESET_PWD_CONFIRMATION_SUBJECT = 'Reset Password Confirmation ✔';
---
+++ mail-service/src/mail/resetPassword.job.dto.ts
@@ -0,0 +1,4 @@
+export interface ResetPasswordJobDto {
+  username: string;
+  email: string;
+}
